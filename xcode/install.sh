18aab4ff839dfa8e6950f9641edc9327ae7270b6
Zachary Waldowski <zach@waldowski.me>
2014-11-02 01:59:23 -0400

    Xcode themes

---
+++ xcode/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Xcode
+#
+# Symlinks Xcode settings on Darwin
+
+# Check for Darwin
+if [ "$(uname -s)" == "Darwin" ]
+then
+  echo "  Symlinking Xcode preferences"
+  mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes
+  cd~/Library/Developer/Xcode/UserData
+  rm -rf FontAndColorThemes
+  ln -s ~/.dotfiles/xcode/themes FontAndColorThemes
+fi
+
+exit 0
