3143435c8e077d0e27e37b0b937482d8833e5a72
Zachary Waldowski <zach@waldowski.me>
2014-11-02 00:37:59 -0400

    Remove homebrew firstinstalls

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,7 +12,4 @@ then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
 fi
 
-# Install homebrew packages
-brew install grc coreutils spark
-
 exit 0
