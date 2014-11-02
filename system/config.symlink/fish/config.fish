36182f2db9b8625fa19571de1757d203b605bd46
Zachary Waldowski <zach@waldowski.me>
2014-11-02 01:57:58 -0400

    Add fish configuration

---
+++ system/config.symlink/fish/config.fish
@@ -0,0 +1,30 @@
+set -x EDITOR "subl -n -w"
+set -x GIT_EDITOR "subl -n -w"
+set -gx RBENV_ROOT /usr/local/opt/rbenv
+
+function ql
+    qlmanage -p $argv >& /dev/null
+end
+
+function p
+    cd ~/Documents/Projects/$1
+end
+
+function w
+    cd ~/Documents/Work/$1
+end
+
+# Path to your oh-my-fish.
+set fish_path $HOME/.dotfiles/fish/oh-my-fish
+set fish_custom $HOME/.dotfiles/fish/custom
+
+# Theme
+set fish_theme zw
+set fish_greeting ""
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+set fish_plugins brew extract rbenv android-sdk subl
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
