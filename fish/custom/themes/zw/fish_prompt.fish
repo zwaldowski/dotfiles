c8cc5de7c785d45497284acd9a71fe6a002239b2
Zachary Waldowski <zach@waldowski.me>
2015-05-25 13:36:56 -0400

    Minor fish theme change (yellow when Git dirty)

--- fish/custom/themes/zw/fish_prompt.fish
+++ fish/custom/themes/zw/fish_prompt.fish
@@ -1,31 +1,19 @@
 function fish_prompt
 
-  if not set -q -g __fish_zw_functions_defined
-    set -g __fish_zw_functions_defined
-    function _git_branch_name
-      echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-    end
-
-    function _is_git_dirty
-      echo (command git status -s --ignore-submodules=dirty ^/dev/null)
-    end
-  end
-
   set -l bold_green (set_color -o green --bold)
   set -l bold_blue (set_color -o blue --bold)
+  set -l bold_yellow (set_color -o yellow --bold)
   set -l normal (set_color normal)
 
   set -l cwd $bold_green(basename (prompt_pwd))
 
-  if [ (_git_branch_name) ]
-    set -l git_branch (_git_branch_name)
-    set -l git_status " ✓"
+  if git rev-parse --is-inside-work-tree > /dev/null 2>&1
+    set -l git_branch (git symbolic-ref --short HEAD)
+    set git_info "$bold_yellow($git_branch ✗)"
 
-    if [ (_is_git_dirty) ]
-      set -l git_status " ✗"
+    if git diff-index --no-ext-diff --quiet HEAD > /dev/null 2>&1
+      set git_info "$bold_blue($git_branch ✓)"
     end
-
-    set git_info "$bold_blue($git_branch$git_status)"
   end
 
   echo -n -s $cwd $git_info $normal ' '
