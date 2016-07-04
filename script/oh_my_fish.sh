79f117083791b107337612bd9c88c480acba5228
Zachary Waldowski <zach@waldowski.me>
2016-07-04 13:09:31 -0400

    [Fish] Setup oh-my-fish outside of Git

--- script/oh_my_fish.sh
+++ script/oh_my_fish.sh
@@ -2,5 +2,6 @@
 set -eu
 . "$(dirname "$0")/common.sh"
 
-echo "Installing Fish packages..."
+echo "Installing Fish and packages..."
+env OMF_CONFIG=~/.cider/fish curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
 fish -c "omf install"
--- symlinks/fish/config.fish
+++ symlinks/fish/config.fish
@@ -1,15 +1,8 @@
-# Path to your oh-my-fish.
-set -g OMF_PATH $HOME/.cider/fish/oh-my-fish
+# Customize Oh My Fish configuration path.
+set -gx OMF_CONFIG $HOME/.cider/fish/config
 
-# Path to your oh-my-fish configuration.
-set -g OMF_CONFIG $HOME/.cider/fish/config
-
+# Reconfigure rbenv to point at Homebrew.
 set -gx RBENV_ROOT /usr/local/opt/rbenv
-
-### Configuration required to load oh-my-fish ###
-# Note: Only add configurations that are required to be set before oh-my-fish is loaded.
-# For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
-# to create a custom plugin instead.
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
