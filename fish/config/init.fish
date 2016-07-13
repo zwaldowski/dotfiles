a3b891a5bdc2f76d6ec7af315788598311a97ad3
Zachary Waldowski <zach@waldowski.me>
2016-07-12 20:19:05 -0400

    [Fish] No more symlinks

--- fish/config/init.fish
+++ fish/config/init.fish
@@ -1,5 +1,6 @@
 set -x EDITOR "subl -n -w"
 set -x GIT_EDITOR "subl -n -w"
+set -gx RBENV_ROOT /usr/local/opt/rbenv
 
 function ql
     qlmanage -p $argv >& /dev/null
--- symlinks/fish/config.fish
+++
@@ -1,8 +0,0 @@
-# Customize Oh My Fish configuration path.
-set -gx OMF_CONFIG $HOME/.cider/fish/config
-
-# Reconfigure rbenv to point at Homebrew.
-set -gx RBENV_ROOT /usr/local/opt/rbenv
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
