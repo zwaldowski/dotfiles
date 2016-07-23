262aa9cccb371cd48345e10624969ba4d9fde6ff
Zachary Waldowski <zach@waldowski.me>
2016-07-23 15:06:28 -0400

    [Fish] Civic theme

--- fish/config/themes/zw/fish_greeting.fish
+++ fish/config/themes/zw/fish_greeting.fish
@@ -1,2 +1,20 @@
 # Remove fish default greeting
 set --erase fish_greeting
+
+# Reconfigure fish colors
+# set -g fish_color_normal normal
+# set -g fish_color_command 005fd7 purple
+set -g  fish_color_command cyan
+set -g fish_color_quote brred
+# set -g fish_color_redirection normal
+# set -g fish_color_end; green
+# set -g fish_color_error red --bold
+# set -g fish_color_param 00afff cyan
+set -g fish_color_comment green
+# set -g fish_color_operator cyan
+# set -g fish_color_escape cyan
+set -g fish_color_cwd --bold green
+# set -g fish_color_autosuggestion 555 yellow
+# set -g fish_color_user red
+set -g fish_color_host --bold blue
+set -g fish_color_host_alt --bold yellow
--- fish/config/themes/zw/fish_prompt.fish
+++ fish/config/themes/zw/fish_prompt.fish
@@ -1,21 +1,15 @@
 function fish_prompt
-
-  set -l bold_green (set_color -o green --bold)
-  set -l bold_blue (set_color -o blue --bold)
-  set -l bold_yellow (set_color -o yellow --bold)
-  set -l normal (set_color normal)
-
-  set -l cwd $bold_green (basename (prompt_pwd))
+  set -l cwd (set_color $fish_color_cwd) (basename (prompt_pwd))
 
   if git_is_repo
     set -l git_branch (git symbolic-ref --short HEAD ^ /dev/null)
 
     if git_is_touched
-      echo -n -s $cwd $bold_yellow "(" $git_branch " ✗)" $normal " "
+      echo -n -s $cwd (set_color $fish_color_host_alt) "(" $git_branch " ✗)" (set_color normal) " "
     else
-      echo -n -s $cwd $bold_blue "(" $git_branch " ✓)" $normal " "
+      echo -n -s $cwd (set_color $fish_color_host) "(" $git_branch " ✓)" (set_color normal) " "
     end
   else
-    echo -n -s $cwd $normal " "
+    echo -n -s $cwd (set_color normal) " "
   end
 end
