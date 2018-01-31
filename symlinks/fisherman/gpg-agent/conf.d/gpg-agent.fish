fdf7dbf81f9620e2c340f29684ae3566a410cd35
Zachary Waldowski <zach@waldowski.me>
2018-01-31 01:26:00 -0500

    Update gpg-agent launcher for gpg 2.2

--- symlinks/fisherman/gpg-agent/conf.d/gpg-agent.fish
+++ symlinks/fisherman/gpg-agent/conf.d/gpg-agent.fish
@@ -1,27 +1,9 @@
 function __launch_gpg_agent -d "Launch GPG agent"
-	gpg-agent --daemon --no-grab --write-env-file ~/.gnupg/.gpg-agent-info >/dev/null
+	gpg-agent --daemon >/dev/null
 end
 
-function __refresh_gpg_agent_info -d "Re-load GPG agent info into environment"
-	cat ~/.gnupg/.gpg-agent-info | sed 's/=/ /' | while read key value
-		set -e $key
-		set -U -x $key "$value"
-	end
+if test -f ~/.gnupg/S.gpg-agent.ssh
+	__launch_gpg_agent
 
 	set -gx GPG_TTY (tty)
-end
-
-if not set -q -x GPG_AGENT_INFO
-	__launch_gpg_agent
-end
-
-if test -f ~/.gnupg/.gpg-agent-info
-	__refresh_gpg_agent_info
-
-	gpg-connect-agent /bye ^/dev/null
-	if test $status -eq 1
-		pkill -U $USER gpg-agent
-		__launch_gpg_agent
-		__refresh_gpg_agent_info
-	end
 end
