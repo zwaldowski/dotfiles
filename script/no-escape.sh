ebce492047bac2290c3e5daa8c39e5fcafaef7bd
Zachary Waldowski <zach@waldowski.me>
2018-02-18 11:33:30 -0500

    Update defaults

---
+++ script/no-escape.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -eu
+. "$(dirname "$0")/common.sh"
+
+echo "Binding Caps Lock to Escape"
+defaults -currentHost write -globalDomain "com.apple.keyboard.modifiermapping.1452-631-0" -array '{ HIDKeyboardModifierMappingDst = 30064771113; HIDKeyboardModifierMappingSrc = 30064771129; }'
+defaults -currentHost write -globalDomain "com.apple.keyboard.modifiermapping.1452-632-0" -array '{ HIDKeyboardModifierMappingDst = 30064771113; HIDKeyboardModifierMappingSrc = 30064771129; }'
