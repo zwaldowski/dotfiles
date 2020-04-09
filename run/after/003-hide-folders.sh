79f696185a11542ec5b3e3dacfb970501e8d41a8
Zachary Waldowski <zach@waldowski.me>
2020-04-09 19:28:37 -0400

    Hide unnecessary folders

---
+++ run/after/003-hide-folders.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Hide undesired folders in ~/, including ~/bin/
+set -eu
+
+echo "Hiding folders…"
+/usr/bin/chflags hidden ~/bin
+/usr/bin/chflags hidden ~/Movies
+/usr/bin/chflags hidden ~/Public
