6c17c94f6fc2f41c4206ca2f61991b23d3ec7e9c
Zachary Waldowski <zach@waldowski.me>
2020-04-27 12:42:09 -0400

    Adjust terminal editor

--- fish/zw/conf.d/000-before.fish
+++ fish/zw/conf.d/000-before.fish
@@ -1,5 +1,5 @@
 set -gx RBENV_ROOT /usr/local/var/rbenv
-set -gx EDITOR "subl -n -w"
-set -gx GIT_EDITOR "subl -n -w"
+set -gx EDITOR "code --wait"
+set -gx GIT_EDITOR "code --wait"
 set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=/usr/local/opt/libressl CFLAGS=-DHAVE_OPAQUE_OPENSSL"
 alias cat="bat"
