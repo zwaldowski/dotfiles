c098d6b6df67f4adde64f1bc1289b8393b393ff2
Zachary Waldowski <zach@waldowski.me>
2020-05-14 17:13:47 -0400

    Don't both exporting rbenv configuration, bootstrap script should do it

--- fish/zw/conf.d/000-before.fish
+++ fish/zw/conf.d/000-before.fish
@@ -1,5 +1,4 @@
 set -gx RBENV_ROOT /usr/local/var/rbenv
 set -gx EDITOR "code --wait"
 set -gx GIT_EDITOR "code --wait"
-set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=/usr/local/opt/libressl CFLAGS=-DHAVE_OPAQUE_OPENSSL"
 alias cat="bat"
