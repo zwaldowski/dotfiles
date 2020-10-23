1b2d0a88b532d942f1114194cb53ef77208391ed
Zachary Waldowski <zach@waldowski.me>
2020-10-22 23:03:42 -0400

    Set GOPATH

--- fish/zw/conf.d/000-before.fish
+++ fish/zw/conf.d/000-before.fish
@@ -1,4 +1,6 @@
 set -gx RBENV_ROOT /usr/local/var/rbenv
 set -gx EDITOR "code --wait"
 set -gx GIT_EDITOR "code --wait"
+set -gx GOPATH $HOME/go
+set -gx PATH $HOME/bin $GOPATH/bin $PATH
 alias cat="bat"
