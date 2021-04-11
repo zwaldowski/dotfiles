eb951a02ac2daae4e12141776574e322e43d39b8
Zachary Waldowski <zach@waldowski.me>
2021-04-11 17:10:44 -0400

    brew: make work on arm64

--- fish/zw/conf.d/000-before.fish
+++ fish/zw/conf.d/000-before.fish
@@ -1,4 +1,10 @@
-set -gx EDITOR "code --wait"
-set -gx GIT_EDITOR "code --wait"
-set -gx PATH $HOME/bin $PATH
+if test -x /opt/homebrew/bin/brew
+  eval (/opt/homebrew/bin/brew shellenv)
+else if test -x /usr/local/bin/brew
+  eval (/usr/local/bin/brew shellenv)
+end
+
+set -x PATH $HOME/bin $HOMEBREW_PREFIX/opt/ruby/bin $PATH
+set -x EDITOR "code --wait"
+set -x GIT_EDITOR "code --wait"
 alias cat="bat"
