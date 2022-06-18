c0ccc4ca677f2f75764c01a41e5cd13d2f662c6e
Zachary Waldowski <zach@waldowski.me>
2022-06-18 10:13:47 -0400

    Tower: Allow VSCode diff to work on M1

--- symlinks/tower/Library/Application Support/com.fournova.Tower3/CompareTools/code.sh
+++ symlinks/tower/Library/Application Support/com.fournova.Tower3/CompareTools/code.sh
@@ -4,7 +4,11 @@ LOCAL="$1"
 REMOTE="$2"
 MERGED="$4"
 
-CMD=/usr/local/bin/code
+CMD=/opt/homebrew/bin/code
+
+if [ ! -x "$CMD" ]; then
+    CMD=/usr/local/bin/code
+fi
 
 if [ -n "$MERGED" ]; then
     "$CMD" --wait "$MERGED"
