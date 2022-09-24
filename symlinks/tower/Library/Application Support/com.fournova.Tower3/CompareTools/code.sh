ac489daa1aeea1ba495def7d62f430552c8c73c9
Zachary Waldowski <zach@waldowski.me>
2022-09-24 11:00:23 -0400

    Git: Update mergetools

--- symlinks/tower/Library/Application Support/com.fournova.Tower3/CompareTools/code.sh
+++ symlinks/tower/Library/Application Support/com.fournova.Tower3/CompareTools/code.sh
@@ -2,7 +2,7 @@
 
 LOCAL="$1"
 REMOTE="$2"
-MERGED="$4"
+MERGING="$4"
 
 CMD=/opt/homebrew/bin/code
 
@@ -10,8 +10,10 @@ if [ ! -x "$CMD" ]; then
     CMD=/usr/local/bin/code
 fi
 
-if [ -n "$MERGED" ]; then
-    "$CMD" --wait "$MERGED"
+if [ -n "$MERGING" ]; then
+	BASE="$3"
+	MERGE="$4"
+    "$CMD" --wait --merge "$LOCAL" "$REMOTE" "$BASE" "$MERGE"
 else
     "$CMD" --wait --diff "$LOCAL" "$REMOTE"
 fi
