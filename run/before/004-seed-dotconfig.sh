41e26e6e56bc091c9bb31c9c9e9ae5e231a57820
Zachary Waldowski <zach@waldowski.me>
2022-12-27 18:21:25 -0500

    run: fix Tower symlinks

--- run/before/004-seed-dotconfig.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-
-echo "Seeding .config"
-
-mkdir -p ~/.config/fish
---
+++ run/before/004-seed-symlinks.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -eu
+
+echo "Seeding symlink folders"
+
+mkdir -p ~/.config/fish
+mkdir -p ~/Library/Application\ Support/com.fournova.Tower3
