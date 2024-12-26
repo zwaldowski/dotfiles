816db23fe79864cbf48b0d6ac6df78d939bce95c
Zachary Waldowski <zach@waldowski.me>
2024-12-26 15:01:16 -0800

    Update install scripts

--- run/before/002-control-strip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Customize the little dude in the top right of the Touch Bar.
-# Add the undocumented "media next" key, replace Siri with Screen Lock.
-set -eu
-
-echo "Adjusting Control Strip…"
-
-defaults write com.apple.controlstrip MiniCustomized -array com.apple.system.brightness com.apple.system.volume com.apple.system.media-next com.apple.system.screen-lock
