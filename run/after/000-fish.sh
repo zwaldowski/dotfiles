930c7f4113b82692a2adb76af1fb096717c30634
Zachary Waldowski <zach@waldowski.me>
2025-10-29 01:14:37 -0700

    Set up Fish without modifying etc

--- run/after/000-fish.sh
+++ run/after/000-fish.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Run "Fisher" package manager for Fish.
+# Set up Fish and "Fisher" package manager for Fish.
 set -eu
 
+sudo dscl . -create /Users/zw UserShell /opt/homebrew/bin/fish
 fish -c "fisher update"
