d8c86596341b6ad798bf3038ab153fe12c60b035
Zachary Waldowski <zach@waldowski.me>
2026-04-18 19:55:48 -0700

    signing fun continues

--- fish/zw/conf.d/000-before.fish
+++ fish/zw/conf.d/000-before.fish
@@ -6,7 +6,7 @@ end
 
 fish_add_path $HOME/bin
 fish_add_path $HOMEBREW_PREFIX/opt/ruby/bin
-fish_add_path $HOMEBREW_PREFIX/lib/ruby/gems/3.4.0/bin
+fish_add_path $HOMEBREW_PREFIX/lib/ruby/gems/4.0.0/bin
 fish_add_path $HOMEBREW_PREFIX/opt/rustup/bin
 alias docker="podman"
 set -gx EDITOR "zed -w"
@@ -15,3 +15,5 @@ set -g fish_color_command brcyan
 set -g hydro_color_pwd 00AFFF
 set -g hydro_color_git brgreen
 set -g hydro_color_prompt $fish_color_command
+
+ssh-add -q
